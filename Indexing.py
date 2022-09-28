1f16d4a33f95316b5f1a4025d8a3c21d07200ec5
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-28 20:42:26 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,19 @@
+# Indexing In Python 
+# In Indexing we use Square brackets "[]" to Print Index
+# Indexing Is Start From Zero
+# Ex. In "Hello" Indexing is h=0,e=1,l=2,l=3,o=4
+
+Index = "Hello"
+print(Index[0])
+print(Index[1])
+print(Index[2])
+print(Index[3])
+print(Index[4])
+
+# Slicing Of String
+
+print(Index[0:1])
+print(Index[0:2])
+print(Index[0:3])
+print(Index[0:4])
+print(Index[0:5])
